c05288bcdfa403a9b5f47d30d43a8126dceda307
mert <uzeyirmertciflikli@gmail.com>
2020-07-08 08:48:04 +0200

    added CharacterGrid component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 import axios from 'axios';
 
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
